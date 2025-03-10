b4ed1128803f70a255ab8a04e5844eae1e9cb7bf
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2025-03-10 15:43:23 +0600

    animations added

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import service_business from "../assets/images/services/service_business_setup.jpg";
 import service_expat from "../assets/images/services/service_expat.jpg";
 import service_jplang from "../assets/images/services/service_jp_language.jpg";
@@ -39,21 +40,30 @@ const servicesData = [
 
 const Services = () => {
   return (
-    <section className="container relative flex flex-col justify-between h-full max-w-7xl px-10 mx-auto xl:px-0 mt-10 text-center">
+    <section className="container relative flex flex-col justify-between h-full max-w-7xl px-10 mx-auto xl:px-0 mt-10 text-center overflow-x-hidden">
       {/* Title & Description */}
-      <h2 className="mt-10 mb-5 text-3xl font-extrabold text-gray-900">Our Services</h2>
+      <h2 className="mt-10 mb-5 text-3xl font-extrabold text-gray-900">
+        Our Services
+      </h2>
       <p className="lg:mx-12 md:mx-8 mb-6 md:text-lg lg:text-xl text-gray-500 dark:text-gray-400 font-serif text-justify">
         At JBA, we offer hassle-free immigration support, talent acquisition,
         business setup, and corporate solutions. We ensure seamless transitions
-        for expatriates, professionals, and businesses, providing expert guidance
-        in the Japanese and Bangladeshi markets.
+        for expatriates, professionals, and businesses, providing expert
+        guidance in the Japanese and Bangladeshi markets.
       </p>
 
       {/* Services List */}
-      <div className="w-full flex flex-col items-center md:py-4 py-10">
+      <div className="w-full scale-85 flex flex-col items-center md:py-4 py-10">
         {servicesData.map((service, index) => (
-          <div
+          <motion.div
             key={index}
+            initial={{
+              opacity: 0,
+              transform: `translateX(${index % 2 === 0 ? "-100%" : "100%"})`,
+            }}
+            whileInView={{ opacity: 1, transform: "translateX(0)" }}
+            transition={{ duration: 0.8, ease: "easeOut" }} // Reduce duration
+            viewport={{ once: true, margin: "-25% 0% -25% 0%" }} // Keeps trigger at center
             className={`xl:w-[90%] sm:w-[85%] w-[90%] mx-auto flex md:flex-row flex-col lg:gap-4 gap-2 justify-center lg:items-stretch md:items-center mt-6 ${
               index % 2 === 1 ? "md:flex-row-reverse" : ""
             }`}
@@ -65,13 +75,15 @@ const Services = () => {
               alt={service.title}
             />
             {/* Service Description */}
-            <div className="md:w-[50%] w-full bg-gradient-to-r from-purple-600 to-pink-600 shadow-lg p-8 md:p-4 rounded-md ">
+            <div className="md:w-[50%] w-full bg-gradient-to-r from-purple-600 to-pink-600 shadow-lg p-8 md:p-4 rounded-md">
               <h3 className="text-3xl mt-8 mb-4 font-semibold font-serif text-white dark:text-white">
                 {service.title}
               </h3>
-              <p className="lg:text-lg md:text-sm mt-4 mb-4 px-4 font-serif font-medium text-justify text-white">{service.description}</p>
+              <p className="lg:text-lg md:text-sm mt-4 mb-4 px-4 font-serif font-medium text-justify text-white">
+                {service.description}
+              </p>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </section>
